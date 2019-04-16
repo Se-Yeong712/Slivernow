442161506c562f308ccf8fe5328f7dbec6410400
Se-Yeong712 <lsysy712@gmail.com>
2019-04-16 23:51:52 +0900

    current

--- app/src/main/java/s2017s16/kr/hs/mirim/silvernow/CurrentActivity.java
+++ app/src/main/java/s2017s16/kr/hs/mirim/silvernow/CurrentActivity.java
@@ -1,6 +1,8 @@
 package s2017s16.kr.hs.mirim.silvernow;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,9 +10,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,7 +25,7 @@ import java.util.List;
 
 public class CurrentActivity extends AppCompatActivity {
     Toolbar toolbar;
-    Button database;
+    TextView day,ex,set;
 
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
@@ -39,10 +44,36 @@ public class CurrentActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
 
 
+
         firebaseAuth = FirebaseAuth.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("");
 
+        child = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        }
 
 ;
 

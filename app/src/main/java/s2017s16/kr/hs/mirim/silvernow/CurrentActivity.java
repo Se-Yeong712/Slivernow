9a09059718ee30c0f4da45f702e3c1647c364b7b
Yewon Kim <kyw017763@gmail.com>
2019-04-18 14:06:32 +0900

    Add bar graph to CurrentActivity

--- app/src/main/java/s2017s16/kr/hs/mirim/silvernow/CurrentActivity.java
+++ app/src/main/java/s2017s16/kr/hs/mirim/silvernow/CurrentActivity.java
@@ -13,12 +13,23 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class CurrentActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -28,6 +39,8 @@ public class CurrentActivity extends AppCompatActivity {
     DatabaseReference databaseReference;
     FirebaseDatabase Database;
     ChildEventListener child;
+
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +83,57 @@ public class CurrentActivity extends AppCompatActivity {
 
             }
         };
+
+        ArrayList<String> labelList = new ArrayList<String>();
+        ArrayList<Integer> valList = new ArrayList<Integer>();
+
+        labelList.add("4/15");
+        labelList.add("4/16");
+        labelList.add("4/17");
+        labelList.add("4/18");
+        labelList.add("4/19");
+        labelList.add("4/20");
+        labelList.add("4/21");
+
+        valList.add(5);
+        valList.add(7);
+        valList.add(6);
+        valList.add(2);
+        valList.add(7);
+        valList.add(1);
+        valList.add(2);
+
+        BarChartGraph(labelList, valList);
+        //List listA = new ArrayList();
+
     }
+
+    private void BarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
+        // BarChart 메소드
+        BarChart barChart = findViewById(R.id.current_chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for(int i=0; i < valList.size();i++){
+            entries.add(new BarEntry((Integer) valList.get(i), i));
+        }
+
+        BarDataSet depenses = new BarDataSet (entries, "날짜별 운동현황"); // 변수로 받아서 넣어줘도 됨
+        depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+        depenses.setValueTextSize(10);
+
+        ArrayList<String> labels = new ArrayList<String>();
+        for(int i=0; i < labelList.size(); i++){
+            labels.add((String) labelList.get(i));
+        }
+
+        BarData data = new BarData(labels,depenses); // 라이브러리 v3.x 사용하면 에러 발생함
+        depenses.setColors(ColorTemplate.COLORFUL_COLORS); //
+
+        barChart.setData(data);
+        barChart.animateXY(1000,1000);
+        barChart.invalidate();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {

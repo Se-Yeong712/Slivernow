9d78479b3d6baab5d822a4c4dfc0e7dbd1985d47
GsDaisy <42744032+GsDaisy@users.noreply.github.com>
2019-05-30 20:00:07 +0900

    운동마당 gif 및

--- app/src/main/java/s2017s16/kr/hs/mirim/silvernow/EyeExerciseActivity.java
+++ app/src/main/java/s2017s16/kr/hs/mirim/silvernow/EyeExerciseActivity.java
@@ -2,6 +2,7 @@ package s2017s16.kr.hs.mirim.silvernow;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,9 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 
@@ -44,6 +47,8 @@ public class EyeExerciseActivity extends AppCompatActivity {
         e.printStackTrace();
     }*/
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +60,7 @@ public class EyeExerciseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         android.support.v7.app.ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+
 
         mediaPlayer = MediaPlayer.create(EyeExerciseActivity.this, R.raw.music);
         mediaPlayer.start();
@@ -82,19 +88,38 @@ public class EyeExerciseActivity extends AppCompatActivity {
         });
 
 
-        /*
+
         ImageView gif = findViewById(R.id.gif);
         GlideDrawableImageViewTarget target = new GlideDrawableImageViewTarget(gif);
-        Glide.with(this).load(R.raw.eye).into(gif);
+        Glide.with(this).load(R.raw.eye_roll).into(gif);
 
-        ImageView count_gif = findViewById(R.id.count_gif);
-        GlideDrawableImageViewTarget count_target = new GlideDrawableImageViewTarget(count_gif);
-        Glide.with(this).load(R.raw.eye_count).into(count_gif);
-        */
+//        (new Thread(new Runnable()
+//        {
+//        @Override
+//        public void run() {
+//            final int[] num = {40000};
+//            while (!Thread.interrupted()) {
+//                try {
+//                    Thread.sleep(1000);
+//                    runOnUiThread(new Runnable() // start actions in UI thread
+//                    {
+//
+//                        @Override
+//                        public void run() {
+//                            num[0] -= 1000;
+//                            tv_num_set_times.setText(num[0]);
+//                        }
+//                    });
+//                } catch (InterruptedException e) {
+//                    // ooops
+//                }
+//            }
+//        }
+//    })).start();
+        }//onCreate
 
 
 
-    }
 
     @Override
     protected void onDestroy() {
